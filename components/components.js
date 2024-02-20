4b09d82d3c0acbcf10912ea082f9ce833b12081f
Enij <Enij@users.noreply.github.com>
2024-02-20 13:05:11 +0200

    Create components.js

---
+++ components/components.js
@@ -0,0 +1,12 @@
+import { Header } from './header.js';
+import { Footer } from './footer.js';
+import { Menu } from './menu.js';
+import { NavItem } from './nav-item.js';
+import { Calculator } from './calculator.js';
+import { Overview } from './overview.js';
+import { Payment } from './payment.js';
+import { About } from './about.js';
+import { PageLayout } from './page-layout.js';
+import { TabPanel } from './tabPanel.js';
+
+export { Header, Footer, Menu, NavItem, Calculator, Overview, Payment, About, PageLayout, TabPanel };
